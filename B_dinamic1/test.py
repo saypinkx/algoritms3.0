bc7faab0b897d0609ff975e36b2a393bf40c9982
sixzerx <paprut538@gmail.com>
2023-12-29 12:56:23 +0300

    start 39

--- B_dinamic1/test.py
+++ B_dinamic1/test.py
@@ -1,5 +1,9 @@
+count = 0
 
-class Graph:
-    def __init__(self, value):
-        self.value = value
-        self.neighbors = []
+for i in range(1, 7):
+    for j in range(1, 7):
+        for k in range(1, 7):
+            if i != j and j != k and i != k and (i == 6 or j == 6 or k == 6):
+               count += 1
+
+print(count)
